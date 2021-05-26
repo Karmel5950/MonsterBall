8d4b2de87de914271d3c3c0d99ac679a235a9483
Karmel5950 <gaoang42@outlook.com>
2021-05-26 14:26:17 +0800

    创建Item相关类 创建Block相关类 完成Item和Block相关类的实例化 完成Item和Block相关类的注册

--- src/main/java/com/github/Karmel5950/monsterball/instanceHandler/ItemHandler.java
+++ src/main/java/com/github/Karmel5950/monsterball/instanceHandler/ItemHandler.java
@@ -1,11 +1,23 @@
 package com.github.Karmel5950.monsterball.instanceHandler;
 
 
+import com.github.Karmel5950.monsterball.item.*;
 import net.minecraft.item.Item;
 import java.util.ArrayList;
 import java.util.List;
 
 public class ItemHandler {
     public static final List<Item> items = new ArrayList<Item>();
+
+    Item itemAnimalMonsterBall = new ItemAnimalMonsterBall();
+    Item itemArchitectureMonsterBall = new ItemArchitectureMonsterBall();
+    Item itemBasicMonsterBall = new ItemBasicMonsterBall();
+    Item itemCreeperMonsterBall = new ItemCreeperMonsterBall();
+    Item itemEndeMonsterBall = new ItemEndeMonsterBall();
+    Item itemMonsterHealPotion = new ItemMonsterHealPotion();
+    Item itemSlimeMonsterBall = new ItemSlimeMonsterBall();
+    Item itemSupremeMonsterBall = new ItemSupremeMonsterBall();
+    Item itemUndeadMonsterBall = new ItemUndeadMonsterBall();
+    Item itemVillagerMonsterBall = new ItemVillagerMonsterBall();
 
 }
---
+++ src/main/java/com/github/Karmel5950/monsterball/item/ItemBasicMonsterBall.java
@@ -0,0 +1,14 @@
+package com.github.Karmel5950.monsterball.item;
+
+import com.github.Karmel5950.monsterball.instanceHandler.ItemHandler;
+import net.minecraft.item.Item;
+
+public class ItemBasicMonsterBall extends Item {
+
+    public ItemBasicMonsterBall(){
+        super();
+        ItemHandler.items.add(this);
+    }
+
+
+}
