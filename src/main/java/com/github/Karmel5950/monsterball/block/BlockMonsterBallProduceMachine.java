53134f6968348c01e715aa78ae435097c958efc5
0odabaio0 <215744720@qq.com>
2021-05-29 22:32:25 +0800

    给怪兽球制造机添加模型和贴图

--- src/main/java/com/github/Karmel5950/monsterball/block/BlockMonsterBallProduceMachine.java
+++ src/main/java/com/github/Karmel5950/monsterball/block/BlockMonsterBallProduceMachine.java
@@ -6,7 +6,9 @@ import com.github.Karmel5950.monsterball.instanceHandler.ItemHandler;
 import com.github.Karmel5950.monsterball.item.API.IHasModel;
 import com.github.Karmel5950.monsterball.tileentity.TileEntityMonsterBallProduceMachine;
 import net.minecraft.block.Block;
+import net.minecraft.block.BlockHorizontal;
 import net.minecraft.block.material.Material;
+import net.minecraft.block.properties.PropertyDirection;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
@@ -19,6 +21,7 @@ import net.minecraft.world.World;
 
 public class BlockMonsterBallProduceMachine extends Block implements IHasModel {
     private final String name = "monsterball_produce_machine";
+    public static final PropertyDirection FACING = BlockHorizontal.FACING;
 
     public BlockMonsterBallProduceMachine() {
         super(Material.IRON);
@@ -28,6 +31,42 @@ public class BlockMonsterBallProduceMachine extends Block implements IHasModel {
         this.setTranslationKey(MonsterBall.MODID + "." + name);
         this.setCreativeTab(MonsterBall.creativeTabsMonsterBall);
 
+    }
+
+    public void onBlockAdded(World worldIn, BlockPos pos, IBlockState state)
+    {
+        this.setDefaultFacing(worldIn, pos, state);
+    }
+
+    private void setDefaultFacing(World worldIn, BlockPos pos, IBlockState state)
+    {
+        if (!worldIn.isRemote)
+        {
+            IBlockState iblockstate = worldIn.getBlockState(pos.north());
+            IBlockState iblockstate1 = worldIn.getBlockState(pos.south());
+            IBlockState iblockstate2 = worldIn.getBlockState(pos.west());
+            IBlockState iblockstate3 = worldIn.getBlockState(pos.east());
+            EnumFacing enumfacing = (EnumFacing)state.getValue(FACING);
+
+            if (enumfacing == EnumFacing.NORTH && iblockstate.isFullBlock() && !iblockstate1.isFullBlock())
+            {
+                enumfacing = EnumFacing.SOUTH;
+            }
+            else if (enumfacing == EnumFacing.SOUTH && iblockstate1.isFullBlock() && !iblockstate.isFullBlock())
+            {
+                enumfacing = EnumFacing.NORTH;
+            }
+            else if (enumfacing == EnumFacing.WEST && iblockstate2.isFullBlock() && !iblockstate3.isFullBlock())
+            {
+                enumfacing = EnumFacing.EAST;
+            }
+            else if (enumfacing == EnumFacing.EAST && iblockstate3.isFullBlock() && !iblockstate2.isFullBlock())
+            {
+                enumfacing = EnumFacing.WEST;
+            }
+
+            worldIn.setBlockState(pos, state.withProperty(FACING, enumfacing), 2);
+        }
     }
 
     @Override
