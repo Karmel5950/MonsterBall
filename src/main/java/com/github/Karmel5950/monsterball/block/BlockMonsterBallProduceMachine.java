b003188a28467ff11c046486d76cbbeb0207c2f8
0odabaio0 <215744720@qq.com>
2021-05-31 22:37:18 +0800

    给怪兽球制造机修改模型和贴图

--- src/main/java/com/github/Karmel5950/monsterball/block/BlockMonsterBallProduceMachine.java
+++ src/main/java/com/github/Karmel5950/monsterball/block/BlockMonsterBallProduceMachine.java
@@ -8,20 +8,26 @@ import com.github.Karmel5950.monsterball.tileentity.TileEntityMonsterBallProduce
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockHorizontal;
 import net.minecraft.block.material.Material;
+import net.minecraft.block.properties.PropertyBool;
 import net.minecraft.block.properties.PropertyDirection;
+import net.minecraft.block.state.BlockStateContainer;
 import net.minecraft.block.state.IBlockState;
+import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemBlock;
+import net.minecraft.item.ItemStack;
 import net.minecraft.tileentity.TileEntity;
+import net.minecraft.tileentity.TileEntityFurnace;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.EnumHand;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 
 public class BlockMonsterBallProduceMachine extends Block implements IHasModel {
-    private final String name = "monsterball_produce_machine";
+    private final String name = "produce_machine";
     public static final PropertyDirection FACING = BlockHorizontal.FACING;
+    public static final PropertyBool BURNING = PropertyBool.create("burning");
 
     public BlockMonsterBallProduceMachine() {
         super(Material.IRON);
@@ -30,6 +36,8 @@ public class BlockMonsterBallProduceMachine extends Block implements IHasModel {
         this.setRegistryName(MonsterBall.MODID + ":" + name);
         this.setTranslationKey(MonsterBall.MODID + "." + name);
         this.setCreativeTab(MonsterBall.creativeTabsMonsterBall);
+        this.setDefaultState(this.blockState.getBaseState().withProperty(FACING, EnumFacing.NORTH)
+                .withProperty(BURNING, Boolean.FALSE));
 
     }
 
@@ -67,6 +75,47 @@ public class BlockMonsterBallProduceMachine extends Block implements IHasModel {
 
             worldIn.setBlockState(pos, state.withProperty(FACING, enumfacing), 2);
         }
+    }
+
+    public IBlockState getStateFromMeta(int meta)
+    {
+        EnumFacing enumfacing = EnumFacing.byIndex(meta&3);
+
+        if (enumfacing.getAxis() == EnumFacing.Axis.Y)
+        {
+            enumfacing = EnumFacing.NORTH;
+        }
+        Boolean burning = Boolean.valueOf((meta & 4) != 0);
+
+        return this.getDefaultState().withProperty(FACING, enumfacing).withProperty(BURNING, burning);
+    }
+
+    public int getMetaFromState(IBlockState state)
+    {
+        int facing = state.getValue(FACING).getHorizontalIndex();
+        int burning = state.getValue(BURNING).booleanValue() ? 4 : 0;
+        return facing | burning ;
+    }
+
+    public void onBlockPlacedBy(World worldIn, BlockPos pos, IBlockState state, EntityLivingBase placer, ItemStack stack)
+    {
+        worldIn.setBlockState(pos, state.withProperty(FACING, placer.getHorizontalFacing().getOpposite()), 2);
+
+        if (stack.hasDisplayName())
+        {
+            TileEntity tileentity = worldIn.getTileEntity(pos);
+
+            if (tileentity instanceof TileEntityFurnace)
+            {
+                ((TileEntityFurnace)tileentity).setCustomInventoryName(stack.getDisplayName());
+            }
+        }
+    }
+
+    @Override
+    protected BlockStateContainer createBlockState()
+    {
+        return new BlockStateContainer(this, FACING, BURNING);
     }
 
     @Override
