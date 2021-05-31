d58c4d922e97ec0f863a2c5491f14760da44b6a9
Karmel5950 <gaoang42@outlook.com>
2021-05-31 22:38:13 +0800

    替换了新的球贴图 增加了治疗药水的贴图 增加了球丢出去收服僵尸的功能

--- src/main/java/com/github/Karmel5950/monsterball/entity/EntityBasicMonsterBall.java
+++ src/main/java/com/github/Karmel5950/monsterball/entity/EntityBasicMonsterBall.java
@@ -2,22 +2,38 @@ package com.github.Karmel5950.monsterball.entity;
 
 import com.github.Karmel5950.monsterball.MonsterBall;
 import com.github.Karmel5950.monsterball.instanceHandler.EntityHandler;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityLiving;
 import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.entity.monster.EntityZombie;
 import net.minecraft.entity.passive.EntityChicken;
 import net.minecraft.entity.projectile.EntityThrowable;
+import net.minecraft.init.Items;
+import net.minecraft.inventory.EntityEquipmentSlot;
+import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.util.DamageSource;
+import net.minecraft.util.EnumHand;
 import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.RayTraceResult;
+import net.minecraft.util.text.TextComponentString;
 import net.minecraft.world.World;
+import net.minecraftforge.common.capabilities.ICapabilitySerializable;
 import net.minecraftforge.fml.common.registry.EntityEntryBuilder;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+
 public class EntityBasicMonsterBall extends EntityThrowable {
+    ItemStack itemStackInPlayer;
 
     public EntityBasicMonsterBall(World worldIn) {
         super(worldIn);
     }
     public EntityBasicMonsterBall(World worldIn, EntityLivingBase throwerIn) {
         super(worldIn,throwerIn);
+        itemStackInPlayer = throwerIn.getHeldItem(EnumHand.MAIN_HAND);
     }
     public EntityBasicMonsterBall(World worldIn, double x, double y, double z) {
         super(worldIn,x,y,z);
@@ -28,7 +44,34 @@ public class EntityBasicMonsterBall extends EntityThrowable {
 
             if (result.entityHit != null)
             {
-                result.entityHit.attackEntityFrom(DamageSource.causeThrownDamage(this, this.getThrower()), 0.0F);
+                if (!this.world.isRemote){
+                    NBTTagCompound tag = getItemTagCompound(itemStackInPlayer);
+                    if(!tag.hasKey("monsterBallEntity")){
+                        result.entityHit.attackEntityFrom(DamageSource.causeThrownDamage(this, this.getThrower()), 0.0F);
+                        if(result.entityHit instanceof EntityZombie && ((EntityZombie) result.entityHit).getHealth() < ((EntityZombie) result.entityHit).getMaxHealth() * 0.5){
+                            result.entityHit.setItemStackToSlot(EntityEquipmentSlot.MAINHAND, new ItemStack(Items.IRON_SWORD));
+                            ((EntityZombie) result.entityHit).setHealth(((EntityZombie) result.entityHit).getMaxHealth());
+                            tag.setBoolean("monsterBallEntityIsSpawn",false);
+                            tag.setTag("monsterBallEntity",((EntityZombie)(result.entityHit)).serializeNBT());
+                            tag.setString("monsterBallEntityName",result.entityHit.getName());
+                            tag.setString("monsterBallEntityClass",result.entityHit.getClass().getName());
+                            result.entityHit.setDead();
+                        }
+                    }
+                }
+            }else {
+                if (!this.world.isRemote){
+                    NBTTagCompound tag = getItemTagCompound(itemStackInPlayer);
+                    if(tag.hasKey("monsterBallEntity")) {
+                        EntityZombie monster = new EntityZombie(world);
+                        monster.deserializeNBT(tag.getCompoundTag("monsterBallEntity"));
+                        BlockPos pos = result.getBlockPos();
+                        monster.setPosition(pos.getX(),pos.getY() + 2,pos.getZ());
+                        this.world.spawnEntity(monster);
+                        tag.setBoolean("monsterBallEntityIsSpawn",true);
+                        this.thrower.sendMessage(new TextComponentString("spawn success"));
+                    }
+                }
             }
 
             if (!this.world.isRemote)
@@ -51,5 +94,15 @@ public class EntityBasicMonsterBall extends EntityThrowable {
 
     }
 
+    public static NBTTagCompound getItemTagCompound(ItemStack stack){
+        NBTTagCompound tag;
+        if(stack.hasTagCompound()){
+            tag = stack.getTagCompound();
+        }else{
+            tag = new NBTTagCompound();
+            stack.setTagCompound(tag);
+        }
 
+        return tag;
+    }
 }
--- src/main/java/com/github/Karmel5950/monsterball/item/ItemBasicMonsterBall.java
+++ src/main/java/com/github/Karmel5950/monsterball/item/ItemBasicMonsterBall.java
@@ -4,11 +4,19 @@ import com.github.Karmel5950.monsterball.MonsterBall;
 import com.github.Karmel5950.monsterball.entity.EntityBasicMonsterBall;
 import com.github.Karmel5950.monsterball.instanceHandler.ItemHandler;
 import com.github.Karmel5950.monsterball.item.API.IHasModel;
+import net.minecraft.client.resources.I18n;
+import net.minecraft.client.util.ITooltipFlag;
+import net.minecraft.entity.monster.EntityZombie;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.util.*;
+import net.minecraft.util.text.TextComponentTranslation;
 import net.minecraft.world.World;
+
+import javax.annotation.Nullable;
+import java.util.List;
 
 public class ItemBasicMonsterBall extends Item implements IHasModel {
     private final String name = "basic_monsterball";
@@ -33,20 +41,49 @@ public class ItemBasicMonsterBall extends Item implements IHasModel {
     public ActionResult<ItemStack> onItemRightClick(World worldIn, EntityPlayer playerIn, EnumHand handIn)
     {
         ItemStack itemstack = playerIn.getHeldItem(handIn);
+        NBTTagCompound tag = getItemTagCompound(itemstack);
 
-        if (!playerIn.capabilities.isCreativeMode)
-        {
-            itemstack.shrink(1);
+        if (!tag.getBoolean("monsterBallEntityIsSpawn")){
+            if (!playerIn.capabilities.isCreativeMode)
+            {
+                itemstack.shrink(1);
+            }
+
+            if (!worldIn.isRemote)
+            {
+                EntityBasicMonsterBall entityBasicMonsterBall = new EntityBasicMonsterBall(worldIn, playerIn);
+                entityBasicMonsterBall.shoot(playerIn, playerIn.rotationPitch, playerIn.rotationYaw, 0.0F, 1.5F, 0.0F);
+                worldIn.spawnEntity(entityBasicMonsterBall);
+            }
+        }else {
+            EntityZombie monster = new EntityZombie(worldIn);
+            monster.deserializeNBT(tag.getCompoundTag("monsterBallEntity"));
+            monster.setDead();
+            tag.setBoolean("monsterBallEntityIsSpawn",false);
         }
-
-
-        if (!worldIn.isRemote)
-        {
-            EntityBasicMonsterBall entityBasicMonsterBall = new EntityBasicMonsterBall(worldIn, playerIn);
-            entityBasicMonsterBall.shoot(playerIn, playerIn.rotationPitch, playerIn.rotationYaw, 0.0F, 1.5F, 1.0F);
-            worldIn.spawnEntity(entityBasicMonsterBall);
-        }
-
         return new ActionResult<ItemStack>(EnumActionResult.SUCCESS, itemstack);
+    }
+
+    @Override
+    public void addInformation(ItemStack stack, @Nullable World worldIn, List<String> tooltip, ITooltipFlag flagIn) {
+        super.addInformation(stack, worldIn, tooltip, flagIn);
+        NBTTagCompound tag = getItemTagCompound(stack);
+        if(!tag.hasKey("monsterBallEntity")){
+            tooltip.add(I18n.format("item.monster_ball.itemImformation.entity") + "empty");
+        }else {
+            tooltip.add(I18n.format("item.monster_ball.itemImformation.entity") + tag.getString("monsterBallEntityName"));
+        }
+    }
+
+    public static NBTTagCompound getItemTagCompound(ItemStack stack){
+        NBTTagCompound tag;
+        if(stack.hasTagCompound()){
+            tag = stack.getTagCompound();
+        }else{
+            tag = new NBTTagCompound();
+            stack.setTagCompound(tag);
+        }
+
+        return tag;
     }
 }
